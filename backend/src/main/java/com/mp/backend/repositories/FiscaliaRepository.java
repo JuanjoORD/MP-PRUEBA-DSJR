8ce0f14ceb1b7ee86819df1aae3c16e301e58b48
JuanjoORD <juanjo97.ord@gmail.com>
2022-09-12 04:45:51 -0600

    :fire: removing useless comments

--- backend/src/main/java/com/mp/backend/repositories/FiscaliaRepository.java
+++ backend/src/main/java/com/mp/backend/repositories/FiscaliaRepository.java
@@ -1,7 +1,5 @@
 package com.mp.backend.repositories;
 
-import java.util.ArrayList;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.mp.backend.models.FiscaliaModel;
 
 @Repository
 public interface FiscaliaRepository extends CrudRepository<FiscaliaModel, Long> {
-    // public abstract ArrayList<FiscaliaModel> findById(Integer id);
+    
 }
